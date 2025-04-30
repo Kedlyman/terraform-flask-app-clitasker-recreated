1018dc662419ec8cead3ed51c988e02cc3ca76eb
Denis Kadlec <denis.kadlec8@gmail.com>
2025-04-30 16:30:02 +0000

    refactor(ec2): stop managing key pair via Terraform

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,7 @@
-resource "aws_key_pair" "default" {
-  key_name   = var.key_name
-  public_key = var.public_key_material
-}
+# resource "aws_key_pair" "default" {
+# key_name   = var.key_name
+# public_key = var.public_key_material
+# }
 
 resource "aws_instance" "app" {
   ami                         = var.ami_id
@@ -9,7 +9,7 @@ resource "aws_instance" "app" {
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [var.security_group_id]
   associate_public_ip_address = true
-  key_name                    = aws_key_pair.default.key_name
+  key_name                    = var.key_name
   iam_instance_profile        = var.instance_profile_name
 
   user_data = file("${path.module}/user-data.sh")
