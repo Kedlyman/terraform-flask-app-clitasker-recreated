6b0270146e9eb94b1d2fc2df37b69b9bb4ca8dd6
Kedlyman <152236942+Kedlyman@users.noreply.github.com>
2025-05-17 09:42:57 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
   default_tags {
     tags = {
-      Project     = "CLITasker"
+      Project     = "terraform-flask-app"
       Environment = "Dev"
       ManagedBy   = "Terraform"
     }
