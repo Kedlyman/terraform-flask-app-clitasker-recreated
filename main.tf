6c5af018ecb9dd8c64cb12ccc9038bf1e670dc6a
Kedlyman <152236942+Kedlyman@users.noreply.github.com>
2025-05-18 13:31:58 +0200

    Removed redundant variables

--- main.tf
+++ main.tf
@@ -3,11 +3,6 @@ module "vpc" {
 
   aws_region            = var.aws_region
   project_name          = "terraform-flask-app"
-  vpc_cidr              = "10.0.0.0/16"
-  public_subnet_cidr_1  = "10.0.1.0/24"
-  public_subnet_cidr_2  = "10.0.2.0/24"
-  private_subnet_cidr_1 = "10.0.3.0/24"
-  private_subnet_cidr_2 = "10.0.4.0/24"
 }
 
 data "http" "my_ip" {
